9945e078d1820b34bd541955b96313931021060c
darkprince131 <piladebajyoti131@gmail.com>
2023-03-17 00:37:02 +0530

    first commit

---
+++ src/utils/constants.js
@@ -0,0 +1,42 @@
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import HomeIcon from '@mui/icons-material/Home';
+import CodeIcon from '@mui/icons-material/Code';
+import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
+import SchoolIcon from '@mui/icons-material/School';
+import FaceRetouchingNaturalIcon from '@mui/icons-material/FaceRetouchingNatural';
+import CheckroomIcon from '@mui/icons-material/Checkroom';
+import GraphicEqIcon from '@mui/icons-material/GraphicEq';
+import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
+
+export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
+
+export const categories = [
+  { name: 'New', icon: <HomeIcon />, },
+  { name: 'IGN', icon: <SportsEsportsIcon />, },
+  { name: 'Coding', icon: <CodeIcon />, },
+  { name: 'ReactJS', icon: <CodeIcon />, },
+  { name: 'NextJS', icon: <CodeIcon />, },
+  { name: 'Music', icon: <MusicNoteIcon /> },
+  { name: 'Education', icon: <SchoolIcon />, },
+  { name: 'Podcast', icon: <GraphicEqIcon />, },
+  { name: 'Movie', icon: <OndemandVideoIcon />, },
+  { name: 'Gaming', icon: <SportsEsportsIcon />, },
+  { name: 'Live', icon: <LiveTvIcon />, },
+  { name: 'Sport', icon: <FitnessCenterIcon />, },
+  { name: 'Fashion', icon: <CheckroomIcon />, },
+  { name: 'Beauty', icon: <FaceRetouchingNaturalIcon />, },
+  { name: 'Comedy', icon: <TheaterComedyIcon />, },
+  { name: 'Gym', icon: <FitnessCenterIcon />, },
+  { name: 'Crypto', icon: <DeveloperModeIcon />, },
+];
+
+export const demoThumbnailUrl = 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwallpaper.dog%2Fanime-youtube-wallpapers&psig=AOvVaw2oe3aof3-QDEpNF0Ls2j_H&ust=1679079103385000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCMCwiJ6P4f0CFQAAAAAdAAAAABAE';
+export const demoChannelUrl = 'https://www.youtube.com/@JasiAmvs';
+export const demoVideoUrl = 'https://www.youtube.com/watch?v=HW6o3VYPgOY&ab_channel=JasiAmvs';
+export const demoChannelTitle = 'Jasi Amvs';
+export const demoVideoTitle = 'One of the best AMV creators';
+export const demoProfilePicture = 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fvectors%2Favatar-icon-placeholder-facebook-1577909%2F&psig=AOvVaw1Jsjzj-hut0CSZ05p2ScwM&ust=1679079690091000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCMCksrWR4f0CFQAAAAAdAAAAABAE'
